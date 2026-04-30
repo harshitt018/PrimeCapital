bd703160eade5d6113e37f344f84ddc1a3bbd9c4
harshitt018 <bscit.harshitjaiswal@gmail.com>
2026-04-30 19:39:12 +0530

    fix: removed PORT fallback

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ app.use((err, req, res, next) => {
 });
 
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
